63757d45320cd7248a925f6aee04e8506c086274
Michal Nabielec <michal.nabie@gmail.com>
2019-09-27 12:57:09 +0200

    Add main

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        //FilePartReader filePartReader = new FilePartReader();
+        FilePartReader filePartReader = new FilePartReader("src/main/resources/test.txt",3,3);
+
+        FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer(filePartReader);
+
+        System.out.println(filePartReader.readLines());
+        System.out.println(fileWordAnalyzer.getWordsOrderedAlphabetically());
+        System.out.println("Substring "+fileWordAnalyzer.getWordsContainingSubstring(""));
+        System.out.println("Palindromes "+fileWordAnalyzer.getStringsWhichPalindromes());
+    }
+}
